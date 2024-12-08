5ce4ed60a5a86836292416a7d17541cc0cdc7be2
Avishka <avishkakkoswaththa@gmail.com>
2024-12-08 18:01:00 +0530

    login modal auto display

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'redirect_to_login' => \App\Http\Middleware\RedirectToLogin::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
